52d18308784958709a9d4a5335c5aa4934e74f2c
Divya K Rajan <divyakrajan001@gmail.com>
2020-03-12 17:06:45 +0530

    mutability successfully completed

---
+++ arrayofobject.js
@@ -0,0 +1,10 @@
+//array of objects
+let journal = [
+		{events: ["work","touched trees","cofee","pizza"], 
+		squirrel: false},
+		{events: ["work","icecream","coliflower"]
+		squirrel: false},
+		{events: ["weekend","cycling","peanuts"],
+		squirrel: true}
+		];
+		console.log("properties----> ",Object.keys(journal));
---
+++ mutability.js
@@ -0,0 +1,9 @@
+//mutability
+let obj1 = {value: 100};
+let obj2 = obj1;
+let obj3 = {value: 100};
+console.log(obj1 == obj2);
+cosole.log(obj1 == obj3);
+obj1.value = 15;
+console.log(obj2.value);
+console.log(obj3.value);
