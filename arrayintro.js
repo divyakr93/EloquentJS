a7d99dbe19bd6e580039917044e8bf7321c2e4ff
Divya K Rajan <divyakrajan001@gmail.com>
2020-03-12 15:51:05 +0530

    added array intro

---
+++ arrayintro.js
@@ -0,0 +1,6 @@
+//array introduction 
+let set = [1, 2, 3 , 4, 5];
+console.log(`Array size of ${set}---> ${set.length}`);
+console.log(`second element in ${set} ===> ${set[1]}`);
+console.log(`add new array element 6--> ${set.push(6)} \n ${set}`);
+console.log(`remove the newly added elemnt ---> ${set.pop()} \n ${set}`);
