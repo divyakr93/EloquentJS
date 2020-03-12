9a102b4acdf501b111d35df7672bba173dc29b01
Divya K Rajan <divyakrajan001@gmail.com>
2020-03-12 16:32:19 +0530

    updated objectintro

--- objintro.js
+++ objintro.js
@@ -12,3 +12,11 @@ let obj = { squirrel: false,
         console.log("deer" in obj);
         console.log(obj.deer);
         console.log(`properties of obj--->  ${Object.keys(obj)}`);
+        let objA = {
+            a: 1, b: 2
+        };
+        console.log("first object ", objA);
+        Object.assign(objA,{b: 3,c: 4});
+        console.log("updated object ", objA);
+        console.log("type of [] is----> ",typeof []);
+        console.log("type of objA----> ",typeof objA);      
