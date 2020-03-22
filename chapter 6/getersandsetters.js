ab2b3d7dc807f7fe7af3d0380a9c07e13924cdba
Divya K Rajan <divyakrajan001@gmail.com>
2020-03-22 16:42:21 +0530

    symbol excercise

---
+++ chapter 6/getersandsetters.js
@@ -0,0 +1,11 @@
+//getters and setters
+let variyingsize =
+{
+	get size()
+	{
+		return Math.floor(Math.random() * 100);
+	}
+};
+console.log(variyingsize.size);
+console.log(variyingsize.size);
+console.log(variyingsize.size);
