ad364065cac3d8b2dfc4da8ae3f79a71b5e78496
Divya K Rajan <divyakrajan001@gmail.com>
2020-03-22 17:24:15 +0530

    excercise

--- chapter 6/excercise1.js
+++ chapter 6/excercise1.js
@@ -1,5 +1,26 @@
 //excercise 1 vector type
-class vector
+class Vec
 {
-	constructor
+	constructor(x, y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	plus(anothervec)
+	{
+		return new Vec(this.x + anothervec.x, this.y + anothervec.y)
+	}
+	minus(anothervec)
+	{
+		return new Vec(this.x - anothervec.x, this.y - anothervec.y)
+		
+	}
+	get length()
+	{
+		return Math.sqrt(this.x * this.y * this.y);
+	}
 }
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+console.log(new Vec(3, 4).length);
+
